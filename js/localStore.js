1ca5a35bfb7ebeef04fd2962a5921a878f1e8476
rhj7513 <rhj7513@naver.com>
2022-06-06 21:53:31 +0900

    최종 선물 이미지 출력 수정

--- js/localStore.js
+++ js/localStore.js
@@ -11,15 +11,13 @@ function selectAge(getInput) {
         }
     else if(getInput=='twenty'){
          window.location.href = './Select_Event_twenty.html';  
-    }
+        }
     else if(getInput=='forty'){
         window.location.href = './Select_Event_forty.html';  
-   }
+        }
    else if(getInput=='sixty'){
     window.location.href = './Select_Event_sixty.html';  
-}
-        
-        
+    }       
 }
 
 
@@ -43,6 +41,78 @@ function appendResults(response){
     document.getElementById("1st").innerHTML = response[0].present;
     document.getElementById("2nd").innerHTML = response[1].present;
     document.getElementById("3rd").innerHTML = response[2].present;
+    if(response[0].present=="장난감"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/장난감.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="꽃"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/꽃.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="녹용"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/녹용.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="목걸이"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/목걸이.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="벨트"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/벨트.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="비타민"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/비타민.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="시계"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/시계.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="유산균"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/유산균.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="지갑"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/지갑.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="크림"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/크림.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="향수"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/향수.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
+    else if(response[0].present=="화장품"){
+        var img = document.createElement("img");
+        img.src = "../assets/imgs/화장품.jpg";
+        var src = document.getElementById("header");
+        src.appendChild(img);
+    }
 }
 
 function submitToServer() {
